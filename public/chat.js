ddd15f32110cbdca79c460393aa3a74b64a8fd9d
as6206520-pj <as6206520@gmail.com>
2025-11-03 16:19:02 +0800

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -81,7 +81,7 @@ async function sendMessage() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        messages: chatHistory
+        messages: chatHistory,
  }),
     });
 
